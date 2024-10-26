7c62ead699f82723dc8d046d1cf93d7efd973dfc
bao24h <167046991+BaoNhanh24h@users.noreply.github.com>
2024-10-26 11:45:49 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,15 +6,13 @@ document.getElementById('uploadExcel').addEventListener('change', function(event
         const data = new Uint8Array(event.target.result);
         const workbook = XLSX.read(data, { type: 'array' });
 
-        const sheetName = workbook.SheetNames[0]; // Chọn sheet đầu tiên
+        const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
 
-        // Chuyển đổi sheet thành JSON
         const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-        // Điền dữ liệu từ tệp Excel vào bảng
         const tbody = document.querySelector('#vocabularyTable tbody');
-        tbody.innerHTML = ''; // Xóa dữ liệu cũ
+        tbody.innerHTML = '';
 
         json.forEach((row) => {
             const [koreanWord, vietnameseMeaning] = row;
@@ -29,7 +27,6 @@ document.getElementById('uploadExcel').addEventListener('change', function(event
             tbody.appendChild(newRow);
         });
 
-        // Thêm sự kiện để kiểm tra khi nhấn Enter
         const inputFields = document.querySelectorAll('.inputKorean');
         inputFields.forEach(inputField => {
             inputField.addEventListener('keydown', function(event) {
@@ -56,11 +53,11 @@ function checkAnswers() {
         const resultCell = row.querySelector('.result');
 
         if (inputKorean === koreanWord) {
-            resultCell.textContent = '✔';
+            resultCell.innerHTML = '<i class="fas fa-check"></i>';
             resultCell.classList.add('correct');
             resultCell.classList.remove('incorrect');
         } else {
-            resultCell.textContent = '✘';
+            resultCell.innerHTML = '<i class="fas fa-times"></i>';
             resultCell.classList.add('incorrect');
             resultCell.classList.remove('correct');
         }
